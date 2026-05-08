9e983936f1a2d089ef199a366a57fbfde2dda18d
Daxzyy <122425004+Daxzyy@users.noreply.github.com>
2026-05-08 20:48:52 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,2 +1,2 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-export default { kit: { adapter: adapter() } };
+import adapter from '@sveltejs/adapter-vercel';
+export default { kit: { adapter: adapter({ runtime: 'nodejs20.x' }) } };
